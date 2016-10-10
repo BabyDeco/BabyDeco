16a3b32d8c5ac1925b0bcb2a777884399e5a03bb
nikuhei <i.24zawa@icloud.com>
2016-10-10 09:02:41 +0900

    Initial Commit. by NIkuhei

---
+++ iOS/BabyDeco/BabyDeco/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BabyDeco
+//
+//  Created by 西澤 郁平 on 2016/10/10.
+//  Copyright © 2016年 ikunyan.org. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iOS/BabyDeco/BabyDecoTests/BabyDecoTests.swift
@@ -0,0 +1,36 @@
+//
+//  BabyDecoTests.swift
+//  BabyDecoTests
+//
+//  Created by 西澤 郁平 on 2016/10/10.
+//  Copyright © 2016年 ikunyan.org. All rights reserved.
+//
+
+import XCTest
+@testable import BabyDeco
+
+class BabyDecoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
